95b91f2f245e68a8caed1f0afc702c17604ea399
meters <hackermeters@qq.com>
2012-07-30 23:31:16 +0800

    放出了一些入口

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -74,16 +74,12 @@
 		<p>用户上传图片列表</p>
 		<code><a href="index.php/imglist">index.php/imglist</a></code>
 
+		<p>用户登陆</p>
+		<code><a href="index.php/login">index.php/login</a></code>
+
 		<p>用户上传图片表单</p>
 		<code><a href="index.php/uploadcat">index.php/uploadcat</a></code>
-
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
+		
 
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
 	</div>
