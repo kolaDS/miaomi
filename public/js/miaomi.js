59bee495f2c3d501a5abcb2028c63309b8575a06
kolaDS <wlfoever@126.com>
2012-08-22 13:33:36 +0800

    头部响应式样式微调

--- public/js/miaomi.js
+++ public/js/miaomi.js
@@ -327,7 +327,8 @@ var Miaomi={
 
 	}
 	M.upLoadFile.callback = function(o){
-		console.log(o);
+		//上传成功返回状态
+
 
 	};
 
@@ -343,3 +344,26 @@ var Miaomi={
 	M.upLoadFile();
 })(jQuery,Miaomi);
 
+<div class="item" imgid="<?php echo($item['imgid']);?>">
+		<div class="item-inner">
+			<div class="item-pic">
+				<img src="/miaomi/public/uploads/<?php echo($item['imgname']);?>.jpg" class="J-miaoPic" imgid="<?php echo($item['imgid']);?>" uid="<?php echo($item['uid']); ?>" uname="<?php echo($item['uname']);?>" uurl="<?php echo($item['uurl'])?>" uavatar="<?php echo($item['uavatar']);?>" imgtext="<?php echo($item['imgtext']); ?>" imgdate="<?php echo($item['imgdate']);?>" />
+			</div>
+			<div class="item-info">
+				<p class="item-describe-txt"><?php echo($item['imgtext']);?></p>
+				<p class="item-info-num"><span class="num-like"><span class="num-like-detail" imgid="<?php echo($item['imgid']);?>"><?php echo($item['imglike']);?></span>喜欢</span><span class="num-share">2分享</span></p>
+			</div>
+			<div class="item-describe">
+					<a target="_blank" class="avatar-wrap" href="http://weibo.com/<?php echo($item['uurl']);?>"><img class="avatar" src="<?php echo($item['uavatar']); ?>"/></a>
+
+					<p class="item-upload-info">上传于 <?php echo($item['imgdate']);?></p>
+                <p><?php echo($item['imgid']);?></p>
+			</div>
+			<div class="item-op">
+				<a href="javascript:void(0);" class="ui-icon icon-like"  imgid="<?php echo($item['imgid']);?>">喜欢</a>
+				<a href="javascript:void(0);" class="ui-icon icon-share">分享</a>
+			</div>
+		</div>
+
+		<div class="item-sd"></div>
+	</div>
