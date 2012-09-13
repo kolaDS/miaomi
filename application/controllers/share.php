735ce13222c356ad480bce1f9684e6b5323d836b
meters <hackermeters@qq.com>
2012-09-13 16:31:44 +0800

    新加了分享的控制器

---
+++ application/controllers/share.php
@@ -0,0 +1,25 @@
+<?php
+
+class uploadComment extends CI_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+        
+    }
+
+    public function index()
+    {   
+        
+    }
+
+
+    public function shareImg($imagePath)
+    {
+        session_start();
+      
+    }
+
+    
+}
+?>
